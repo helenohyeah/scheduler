1b518353b1c5fba68fcc317fbb89bd707e7480bd
helenohyeah <ouyang.helen@gmail.com>
2020-10-26 21:39:54 -0400

    Update DayListItem to format spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,13 +8,25 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   })
+
+  const formatSpots = (num) => {
+    switch(num) {
+      case 0:
+        return 'no spots remaining';
+      case 1:
+        return '1 spot remaining';
+      default:
+        return num + ' spots remaining';
+    }
+  };
+
   return (
     <li 
       className={dayListClass}  
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
