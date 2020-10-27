e5d160ba81f5b8470ff7022d7008fd8f86674f14
helenohyeah <ouyang.helen@gmail.com>
2020-10-26 21:30:44 -0400

    Fix import of classnames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from "react";
-import classNames from "classnames";
+import classnames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = classNames("button", {
+   let buttonClass = classnames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger,
    });
