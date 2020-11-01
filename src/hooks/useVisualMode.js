5d1ed7d0a65ba312c617983fb33f634b88bd108b
helenohyeah <ouyang.helen@gmail.com>
2020-11-01 17:36:27 -0500

    Implement condition to prevent navigating back from initial

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
-export default function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
-
-  const transition = (mode) => setMode(mode);
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
   
-  return { mode, transition }
+  const transition = (mode) => {
+    setHistory([...history, mode]);
+    setMode(mode);
+  }
+  
+  const back = () => {
+    // Prevent navigating back from initial mode
+    if (history.length <= 1) {
+      setMode(initial);
+      return;
+    }
+    
+    const prevMode = history[history.length-2];
+    setHistory(history.splice(0, history.length-1));
+    setMode(prevMode);
+  }
+  
+  return { mode, transition, back }
 }
