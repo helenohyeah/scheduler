34e99fc2209ec66ad5f356b2bb32f65bd59cce76
helenohyeah <ouyang.helen@gmail.com>
2020-11-01 16:57:06 -0500

    Create useVisualMode function and implement initial and transition function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+
+  const transition = (mode) => setMode(mode);
+  
+  return { mode, transition }
+}
