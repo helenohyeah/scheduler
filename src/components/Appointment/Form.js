a0d8839628163ebebb251f3eb06b7751e769a61a
helenohyeah <ouyang.helen@gmail.com>
2020-11-03 16:11:59 -0500

    Add student name validation to Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,20 +6,35 @@ import Button from "../Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.value || null);
+  const [error, setError] = useState("");
 
+  // CLEAR INPUT FIELD AND INTERVIEWER IF CANCELED
   const reset = () => {
     setName("");
     setInterviewer(null);
   };
 
+  // CANCEL NEW APPOINTMENT BOOKING
   const cancel = () => {
     reset();
     props.onCancel();
   };
 
+  // CHECK THAT STUDENT NAME IS NOT BLANK
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    // 5 IF NOT BLANKSAVE APPOINTMENT
+    props.onSave(name, interviewer);
+  }
+
   return (
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
+
       <form autoComplete="off" onSubmit={e => e.preventDefault()}>
         <input
           className="appointment__create-input text--semi-bold"
@@ -31,16 +46,21 @@ export default function Form(props) {
           data-testid="student-name-input"
         />
       </form>
+
+      <section className="appointment__validation">{error}</section>
+
       <InterviewerList
         interviewers={props.interviewers}
         value={interviewer}
         onChange={setInterviewer}
       />
+
     </section>
+
     <section className="appointment__card-right">
       <section className="appointment__actions">
         <Button danger onClick={cancel}>Cancel</Button>
-        <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+        <Button confirm onClick={validate}>Save</Button>
       </section>
     </section>
   </main>
