c9064462a1227924bcf125fd388d6a97d5195de9
helenohyeah <ouyang.helen@gmail.com>
2020-11-03 23:30:57 -0500

    Add unit test for Application

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -12,10 +12,12 @@ export default function useApplicationData() {
 
   // GET SERVER DATA ON LOAD
   useEffect(() => {
+    axios.defaults.baseURL = "http://localhost:8001";
+
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(data => {
         setState(prev => ({...prev, days: data[0].data, appointments: data[1].data, interviewers: data[2].data}));
     }).catch(err => console.log(err))
