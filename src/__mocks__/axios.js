d10d1bcd9f32147f686f96c75a8caab7fa1a67a0
helenohyeah <ouyang.helen@gmail.com>
2020-11-04 11:00:50 -0500

    Add integration test for Application

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,5 +80,17 @@ export default {
     };
 
     return response[url];
+  }),
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
   })
 }
