d1f04fc35bf88c1377f056b14aebd6e5e2ed0330
helenohyeah <ouyang.helen@gmail.com>
2020-11-04 16:56:38 -0500

    Add end-to-end test for navigation

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,11 @@
+beforeEach(() => {
+  cy.visit("/");
+})
+
+describe("Navigation", () => {
+  it("should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
